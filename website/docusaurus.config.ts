357d187a57e757dd75924591d08284ceee6fb65c
Tanguy VANSNICK <191451@umons.ac.be>
2025-02-18 20:56:16 +0100

    Initial commit with Docusaurus setup

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -10,15 +10,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://tanguyvans.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'tanguyvans', // Usually your GitHub org/user name.
+  projectName: 'tp-react-native', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
