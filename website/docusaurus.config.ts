3bf23d99ad0e651324d4dcf2302e5e3efd49d346
Tanguy VANSNICK <191451@umons.ac.be>
2025-02-18 22:33:24 +0100

    docs

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://tanguyvans.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tp-react-native/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
